13b2fefc94318367f6611d2c8b2c17d053fe3522
DEEPAK MEENA <55993229+MEMARSAHAB@users.noreply.github.com>
2020-01-30 21:47:12 +0530

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,56 @@
+
+//js for header
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// js for header end
+
+//js for table
+
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+//js for table end
+
+function deflt(){
+    var day = new Date().getDay();
+    preday(day);
+  
+  } 
+function preday(day){
+
+    var pivot = `.day${day}`;
+    // document.querySelector(pivot).className = "menu"
+     document.querySelector(pivot).style.display = "none"
+}
+
+
+
+
+
---
+++ oxys.js
@@ -0,0 +1,4 @@
+function foodimg()
+{
+    document.getElementsByClassName("lunch").style.background-color: rgba(8, 8, 8, 0.842);
+}
